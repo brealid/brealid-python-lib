1ea646ef718f89ce7610b44233d5693397e0245a
brealid <1134204972@qq.com>
2024-07-18 22:37:51 +0800

    update EasyCrypt

--- brealid/ctf/crypto/__init__.py
+++ brealid/ctf/crypto/__init__.py
@@ -1,5 +1,6 @@
-from . import GaloisNumber_2n
+from .GaloisNumber_2n import GaloisNumber_2n
+from .EasyCrypt import EasyCrypt
 
 
 # Alias
-GF2n = GaloisNumber_2n.GaloisNumber_2n
+GF2n = GaloisNumber_2n
